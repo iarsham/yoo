9866077ba73dbf5196c4ff04b8ce5050994640bf
Arsham <arshamdev2001@gmail.com>
2024-07-08 15:24:51 +0330

    add:style to response ask command & add openai client

--- cmd/ask.go
+++ cmd/ask.go
@@ -1,15 +1,23 @@
 package cmd
 
 import (
+	"github.com/iarsham/yoo/internal"
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 )
 
 var askCmd = &cobra.Command{
-	Use:   "ask",
+	Use:   "ask \"question\"",
 	Short: "ask a question and yoo will answer it for you",
 	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Println(args[0])
+	RunE: func(cmd *cobra.Command, args []string) error {
+		pterm.DefaultSpinner.Start("Thinking...📩")
+		resp, err := internal.RspOpenAI(args[0])
+		if err != nil {
+			return err
+		}
+		pterm.DefaultBox.WithTitle("Answer").WithTextStyle(pterm.NewStyle(pterm.FgRed, pterm.BgWhite, pterm.Bold)).Println(resp)
+		return nil
 	},
 }
 
